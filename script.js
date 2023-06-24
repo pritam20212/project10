d9239b8fb5f1ecb1feaf4de4289a07f852e91ff4
pritam20212 <pritamprusty2000@gmail.com>
2023-06-24 23:37:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const signUp = document.getElementById('signup');
+const message =document.getElementById('message');
+
+if(localStorage.getItem('user')){
+    location.href = './profile.html';
+}
+
+function redirectToProfile(){
+    if(localStorage.getItem('token')){
+        location.href='./profile.html';
+    }
+    else{
+        alert('Please Sign Up');
+        location.href = './index.html';
+    }
+}
+
+function redirectToSignUp(){
+    location.href='./index.html';
+}
+
+signUp.addEventListener('click',()=>{
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('pass').value;
+    const conpass = document.getElementById('confirm-pass').value;
+
+    if(name == '' || email == "" || pass == '' || conpass ==''){
+        message.innerHTML="<div style='color:#FF4F4F;'>Error : All feilds are mandatory</div>";
+        return;
+    }
+    
+    if(pass!=conpass)
+    {
+        message.innerHTML="<div style='color:#FF4F4F;'> Error : Passwords should be same</div>";
+        return;
+    }
+
+    let user = {
+        name:name,
+        email:email,
+        password:pass
+    }
+
+    localStorage.setItem("user",JSON.stringify(user));
+
+    let token = '';
+    for(let i = 0;i < 16;i++){
+        token += String.fromCharCode(Math.floor(Math.random() * 256));
+    }
+
+    //store the token in local storage
+    localStorage.setItem('token', btoa(token));
+
+    message.innerHTML="<div style='color:#7ECD71;'>Successfully Signed Up!</div>";
+
+    location.href='./profile.html'
+})
